e5bb40c2cb7f5626c6fd6de6312c60ebcb695fd5
drmswt <978447957@qq.com>
2016-08-13 21:23:59 +0800

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by swtdrm on 16/8/12.
+ */
+var $ = require('jquery');
+
+$(function () {
+    $('#test1').text('wangcan love zhangmengtian');
+
+    require('./index2.js');
+})
---
+++ index2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by swtdrm on 16/8/12.
+ */
+    
+    var $ = require('jquery')
+    $('#test2').text('zhangmengtian loves wangcan');
+
+module.exports = {
+
+}
